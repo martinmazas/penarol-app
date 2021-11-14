b54bd02720e36f1eb7333074b8a3b9b058d74287
martinmazas <mazasmartin@gmail.com>
2021-11-14 09:26:06 +0200

    fixing language icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,8 +13,8 @@ export default function Header() {
     const handleClick = (mode) => {
         setNavValues(0);
         mode === 'logout' ?
-            // axios.get(`https://penarol-app.herokuapp.com/api/user/logout`)
-            axios.get(`http://localhost:3000/api/user/logout`)
+            axios.get(`https://penarol-app.herokuapp.com/api/user/logout`)
+            // axios.get(`http://localhost:3000/api/user/logout`)
                 .then(docs => {
                     if (docs.data === 'Successfully logout') {
                         cookies.remove('token');
@@ -33,12 +33,18 @@ export default function Header() {
         <div className="header" style={styles.root}>
             <img onClick={initialPage} style={styles.logo} src="https://www.xn--pearol-xwa.org/plantillas/images/logo_1.svg" alt="Club Atlético Peñarol" />
             <div className="languages" style={styles.languages}>
-                <img onClick={() => setLanguage('spanish')} src="https://www.countryflags.io/es/flat/32.png" alt="spainFlag" />
-                <img onClick={() => setLanguage('english')} src="https://www.countryflags.io/us/flat/32.png" alt="usaFlag" />
+                <img style={styles.languageIcon} onClick={() => setLanguage('english')} alt="English" src="https://unpkg.com/language-icons/icons/en.svg" />
+                <img style={styles.languageIcon} onClick={() => setLanguage('spanish')} alt="Spanish" src="https://unpkg.com/language-icons/icons/es.svg" />
             </div>
-            <button onClick={() => handleClick(document.cookie ? 'logout' : 'login')} style={styles.login}>
+            {/* <button onClick={() => handleClick(document.cookie ? 'logout' : 'login')} style={styles.login}>
                 {document.cookie ? language === 'english' ? "Log out" : "Salir" : language === 'english' ? "Log in" : "Entrar"}
-            </button>
+            </button> */}
+            {document.cookie ?
+                <button onClick={() => handleClick('logout')} style={styles.login}>
+                    {/* {document.cookie ? language === 'english' ? "Log out" : "Salir" : language === 'english' ? "Log in" : "Entrar"} */}
+                    {language === 'english' ? "Log out" : "Salir"}
+                </button> : null
+            }
         </div>
     );
 }
@@ -59,14 +65,18 @@ const styles = {
         display: 'inline',
         position: 'absolute',
         top: 0,
-        right: 60,
+        right: 0
+    },
+    languageIcon: {
+        width: 25,
+        height: 25,
     },
     login: {
-        backgroundColor: 'black', 
-        color: "white", 
-        top: 6, 
-        position: 'absolute', 
-        display: 'inline', 
-        right: 0
+        backgroundColor: 'black',
+        color: "white",
+        top: 6,
+        position: 'absolute',
+        display: 'inline',
+        right: 70,
     }
 }
